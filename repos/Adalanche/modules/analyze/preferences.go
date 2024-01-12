62dc4b14ea68869841300f02a47f7f9c96a5dcb8
cmahrl <christoph@syslifters.com>
2024-01-12 02:39:29 -0800

    lkarlslund/Adalanche@0161570085c1c8d36a839d7e9591ca76dd214b6f

---
+++ repos/Adalanche/modules/analyze/preferences.go
@@ -0,0 +1,53 @@
+package analyze
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"sync"
+)
+
+type Prefs struct {
+	data map[string]any
+}
+
+var prefmutex sync.Mutex
+
+func (p *Prefs) Load() error {
+	prefmutex.Lock()
+	defer prefmutex.Unlock()
+	p.data = make(map[string]any)
+
+	rawprefs, err := os.ReadFile("preferences.json")
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(rawprefs, &p.data)
+	return err
+}
+
+func (p *Prefs) Save() error {
+	prefmutex.Lock()
+	defer prefmutex.Unlock()
+	rawprefs, err := json.Marshal(p.data)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile("preferences.json", rawprefs, 0600)
+	return err
+}
+
+func (p *Prefs) Set(key string, val any) {
+	prefmutex.Lock()
+	defer prefmutex.Unlock()
+	p.data[key] = val
+}
+
+func (p *Prefs) Get(key string) any {
+	prefmutex.Lock()
+	defer prefmutex.Unlock()
+	return p.data[key]
+}
