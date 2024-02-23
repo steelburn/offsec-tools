f2b76a90cabb1e9554f18e0e4d0adf9a91662351
cmahrl <christoph@syslifters.com>
2024-02-23 11:06:13 -0800

    lkarlslund/Adalanche@3ab9960481656901361a2d113e4968521d3556b5

---
+++ repos/Adalanche/adalanche/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/lkarlslund/adalanche/modules/cli"
+	_ "github.com/lkarlslund/adalanche/modules/integrations/activedirectory/analyze"
+	_ "github.com/lkarlslund/adalanche/modules/integrations/activedirectory/collect"
+	_ "github.com/lkarlslund/adalanche/modules/integrations/localmachine/analyze"
+	_ "github.com/lkarlslund/adalanche/modules/quickmode"
+	"github.com/lkarlslund/adalanche/modules/ui"
+)
+
+func main() {
+	err := cli.Run()
+
+	if err != nil {
+		ui.Error().Msg(err.Error())
+		os.Exit(1)
+	}
+}
