f0893d751ef47aae450c9d379845f04e76c9be78
cmahrl <christoph@syslifters.com>
2024-12-13 11:51:22 -0800

    lkarlslund/Adalanche@e6dba54da4cbd08647b2891dc629f26a859088bb

---
+++ repos/Adalanche/modules/frontend/status.go
@@ -0,0 +1,14 @@
+package frontend
+
+//go:generate go run github.com/dmarkham/enumer -type=WebServiceStatus -output enums.go
+
+type WebServiceStatus int
+
+const (
+	NoData WebServiceStatus = iota
+	Error
+	Loading
+	Analyzing
+	PostAnalyzing
+	Ready
+)
